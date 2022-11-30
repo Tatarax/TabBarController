34ae1ac549bade7a86df18a2e49dde8f2eed5cb6
Tatarax <dinarvaf@icloud.com>
2022-11-30 22:57:46 +0300

    Setting Alert Controller

--- TabBarController/ViewController.swift
+++ TabBarController/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func ShowLoginAC(_ sender: UIButton) {
+        let loginAlertController = UIAlertController(title: "Твой логин", message: "Логин: admin", preferredStyle: .alert)
+        
+        let alertButtom = UIAlertAction(title: "OK", style: .default)
+        loginAlertController.addAction(alertButtom)
+        
+        present(loginAlertController, animated: true)
+    }
+    
+    @IBAction func ShowPasswordAC() {
+        let passwordAlertController = UIAlertController(title: "Твой пароль", message: "Пароль: admin123", preferredStyle: .alert)
+        
+        let alertButtom = UIAlertAction(title: "ОК", style: .default)
+        passwordAlertController.addAction(alertButtom)
+        
+        present(passwordAlertController, animated: true)
+    }
 }
 
